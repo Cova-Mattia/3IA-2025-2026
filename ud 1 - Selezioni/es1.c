13e8bb4dfccc0b1041d500e7e96e9edf0081e6ef
Cova Mattia <mattia.cova@galileiostiglia.edu.it>
2025-11-11 10:42:29 +0000

    Esercizi Salvati

---
+++ ud 1 - Selezioni/es1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+    printf("Ciao\n");
+    return (0);    
+
+}
+/* 1. gcc nomefile.c -> compila il file e può avere 2 file
+        1-> tutto bene viene creato il file a.out (eseguibile)
+        2-> ci sono errori di compilazione (sintassi)
+    2. ./a.out -> esegue il file compilato
+
+    è poossibile unificare i 2 comandi in uno solo:
+    gcc nomefile.c && ./a.out
+
+    ls -> elenca i file nella cartella corrente
+    cd 'nomecartella'/ -> esegue il programma nella cartella specificata
+*/
