4342a931483b48b87ce128b2a782e3425336995f
Cova Mattia <mattia.cova@galileiostiglia.edu.it>
2025-11-18 10:49:44 +0000

    ternapitagorica

---
+++ ud 1 - Selezioni/es3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main () {
+    int n1;
+    int n2;
+    int n3;
+    int terna;
+
+    printf("Inserisci il primo valore:\n");
+    scanf("%d", &n1);
+
+    printf("Inserisci il secondo valore:\n");
+    scanf("%d", &n2);
+
+    printf("Inserisci il terzo valore:\n");
+    scanf("%d", &n3);
+
+    if (n1 >= n2 && n1 >= n3 && n2 >= n3) {
+
+        printf("Sono in ordine crescente\n")
+
+        int terna == n1^2*n2^2
+    }    
+
+    else {
+        printf("I numeri non sono in ordine cresce per ciò nessuna terna pitagorica")
+    }
+
+    if (terna=n3^2){
+        printf("è una terna pitagorica")
+    }
+
+    else {
+        printf("non è une terna")
+    }
+
+    return 0;
+
+}
--- ud 1 - Selezioni/voto.c
+++ ud 1 - Selezioni/voto.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 int main (void) {
-
     int voto;
-    printf("Inserisci il voto:\n")
-    scanf("%d,"voto);
-    printf("%d,"voto)
+    printf("Inserisci il voto:\n");
+    scanf("%d",&voto);
+    if (voto>= 1 && voto<= 3){
+       printf ("gravemente insufficente\n");
+       
+    } else if (voto>= 4 && voto<= 5){
+        printf ("insufficente\n");
+        
+    } else if (voto == 6){
+        printf ("sufficente\n");
+        
+    } else if (voto>= 7 && voto <=8){
+        printf("buono\n");
 
+    }
+ else if (voto>= 9 && voto <=10){
+        printf("eccellente\n");
 
-    return 0
+}
+        return (0);
 }
